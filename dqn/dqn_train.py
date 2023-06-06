b1c98bd46c9ce0cdbe55e1ae74f3c20e4efc26ee
AmiyaSX <2668590277@qq.com>
2023-06-06 16:07:56 +0800

    init

---
+++ dqn/dqn_train.py
@@ -0,0 +1,15 @@
+import xt
+from xt.main import main
+from xt.train import train
+from zeus.common.util.register import Registers
+
+from dqn_cnn_ms import DqnCnnMS
+from dqn_mlp_ms import DqnMlpMS
+from dqn_cnn_pong_ms import DqnCnnPongMS
+from model_ms import XTModel_MS
+
+Registers.model(XTModel_MS)
+Registers.model(DqnCnnMS)
+Registers.model(DqnMlpMS)
+Registers.model(DqnCnnPongMS)
+train()
